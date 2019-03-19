7a5911b979ec59e53885973d19f2c4146d9c9bed
Ross Jourdain <ross.jourdain@gmail.com>
2019-03-20 10:42:55 +1300

    NPM things

---
+++ wk_1_npm/index.js
@@ -0,0 +1,13 @@
+var colors = require('colors');
+var cowsay = require('cowsay');
+
+console.log('Hi there')
+console.log('How are you today?')
+
+console.log('OMG Rainbows!'.rainbow);
+
+console.log(cowsay.saySomthing({
+	text : "Welcome to EDA, you thought coding would be useful....",
+	e : "o0",
+	T : "U "
+}));
